a74be65d9b736bf1674f9e035493f0c866efe898
Danii.O <dani@Daniels-iMac.local>
2018-01-24 23:56:24 +0100

    Segunda Revision P.Integrador
    Segunda revision:
    ListView
    Loguin Firebase
    SignUp Firebase
    Change Password
    Menu desplegable: Opciones user

--- app/src/main/java/com/example/a21624926/proyectointegrador/MainActivity.java
+++ app/src/main/java/com/example/a21624926/proyectointegrador/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.a21624926.proyectointegrador;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -17,6 +19,9 @@ import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.ArrayList;
 
@@ -143,6 +148,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else if (id == R.id.nav_send) {
 
         }
+        else if (id == R.id.nav_Options) {
+
+            startActivity(new Intent(MainActivity.this, OptionsActivity.class));
+
+                    }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
